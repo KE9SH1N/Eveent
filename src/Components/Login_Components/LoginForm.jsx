5f75baa7f9b3e51c74bd30b620bc6d11e6e042e6
unknown <k3nshi9@gmail.com>
2022-10-27 18:01:44 +0600

    adding login form component

--- src/Components/Login_Components/LoginForm.jsx
+++ src/Components/Login_Components/LoginForm.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
 import './LoginForm.scss'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebookSquare } from '@fortawesome/free-brands-svg-icons'
+import { faGooglePlusSquare } from '@fortawesome/free-brands-svg-icons'
+import { faTwitterSquare } from '@fortawesome/free-brands-svg-icons'
+
+import { Link } from 'react-router-dom'
+
+
+
 const LoginForm = () => {
   return (
-    <div>LoginForm</div>
+    <div className='formContainer'>
+      <form action="">
+        <span>Sign in or create an account</span>
+        <input type="text" placeholder='email or phone number' />
+        <input type="password" placeholder='password' />
+        <Link to=""><button type='submit'>login</button></Link>
+      </form>
+
+      <div className='formBottomContent'>
+        <h5>With your social network</h5>
+        <div className="formSocialIcons">
+          <FontAwesomeIcon icon={faFacebookSquare} className='icon facebook' />
+          <FontAwesomeIcon icon={faGooglePlusSquare} className='icon googlePlus' />
+          <FontAwesomeIcon icon={faTwitterSquare} className='icon twitter' />
+        </div>
+        <Link to="" className='regLink'>Not a member? Sign up</Link>
+      </div>
+    </div>
   )
 }
 
