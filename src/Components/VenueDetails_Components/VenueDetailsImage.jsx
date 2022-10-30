1eceace8fac8b38a761736788eb235803994e287
unknown <k3nshi9@gmail.com>
2022-10-30 17:11:48 +0600

    adding venue details image components

---
+++ src/Components/VenueDetails_Components/VenueDetailsImage.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ImageRowOne from './Details_Image/ImageRowOne'
+import ImageRowTwo from './Details_Image/ImageRowTwo'
+
+import './VenueDetailsImage.scss'
+
+
+const VenueDetailsImage = () => {
+  return (
+    <div>
+        <ImageRowOne/>
+        <ImageRowTwo/>
+    </div>
+  )
+}
+
+export default VenueDetailsImage
