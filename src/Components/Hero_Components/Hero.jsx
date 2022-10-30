d2eb66cc170a72584c22e16556304ba97646bc3d
unknown <k3nshi9@gmail.com>
2022-10-30 17:02:06 +0600

    adding hero components

---
+++ src/Components/Hero_Components/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Hero.scss'
+
+
+import Background from "../../Assets/heroBackground.jpg"
+import SearchBar from "./SearchBar"
+
+const Hero = () => {
+  return (
+    <div className='heroContainer'>
+        <img src={Background} alt=""/>
+        <div className="heroheadText">
+            <h3>Search a venue for your function</h3>
+        </div>
+      <SearchBar/>
+    </div>
+  )
+}
+
+export default Hero
